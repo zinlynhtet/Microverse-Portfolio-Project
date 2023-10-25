cba88aa0c2de74b22b01d7fe9214a9e303f2d3e7
MacFlick <mackk5504@gmail.com>
2023-10-25 22:09:12 +0630

    Add js file for the menu bar

---
+++ index.js
@@ -0,0 +1,20 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const navTag = document.querySelector('.navTag');
+const navBag = document.querySelector('.navBag');
+const navHag = document.querySelector('.navHag');
+// eslint-disable-next-line no-use-before-define
+openMenu.addEventListener('click', show);
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+function close() {
+  mainMenu.style.top = '-100%';
+}
+closeMenu.addEventListener('click', close);
+navTag.addEventListener('click', close);
+navBag.addEventListener('click', close);
+navHag.addEventListener('click', close);
